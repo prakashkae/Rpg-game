db74702cc5f7c052bb062a05cd0a9777b796c938
prakashkae <40635221+prakashkae@users.noreply.github.com>
2021-06-25 13:59:36 +0545

    comit

---
+++ RPG Game/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask MovementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        cam = Camera.main;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, MovementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+    }
+}
+
